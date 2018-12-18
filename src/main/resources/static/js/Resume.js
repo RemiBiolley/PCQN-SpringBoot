20ce5bd9793b9c65b08a60940e99ae7e89f2d85b
Nigirith <fanny.streiff@gmail.com>
2018-12-18 17:19:19 +0100

    m

--- src/main/resources/static/js/Resume.js
+++ src/main/resources/static/js/Resume.js
@@ -1,7 +1,5 @@
-function OnOff_1() {
-    console.log(document.getElementById("tips1").style.display);
-    if (document.getElementById("tips1").style.display == "none" || document.getElementById("tips1").style.display == "")
-        document.getElementById("tips1").style.display = "block";
-    else
-        document.getElementById("tips1").style.display = "none";
-}
+$(OnOff1(){
+    $('h2').click(OnOff1(){
+        $('#tonDiv').toggle()
+    });
+});
