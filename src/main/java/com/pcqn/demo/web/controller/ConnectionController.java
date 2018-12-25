dafba14c0aabb7ed45b93cdfd6e21753e6ad1bc1
RemiBiolley <33449916+RemiBiolley@users.noreply.github.com>
2018-12-25 22:38:41 +0100

    Session sur la connexion

--- src/main/java/com/pcqn/demo/web/controller/ConnectionController.java
+++ src/main/java/com/pcqn/demo/web/controller/ConnectionController.java
@@ -10,22 +10,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class ConnectionController {
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping("/connection")
-    public String connectionForm(Model model) {
-        model.addAttribute("connection", new Connection());
-        return "connection";
+    public String connectionForm(Model model, HttpServletRequest request) {
+        String destination;
+        if(request.getSession(false)!=null){
+            System.out.println("1");
+            User user = (User) request.getSession().getAttribute("user");
+            model.addAttribute("name", user.getUserName());
+            model.addAttribute("email", user.getEmail());
+            destination="profil";
+        }
+        else{
+            System.out.println("2");
+            model.addAttribute("connection", new Connection());
+            destination="connection";
+        }
+        return destination;
     }
 
     @PostMapping("/connection")
-    public String connectionSubmit(@ModelAttribute Connection connection, Model model) {
+    public String connectionSubmit(@ModelAttribute Connection connection, Model model, HttpServletRequest request) {
         
         if(userRepository.existsUserByEmailAndPassword(connection.getEmail(), connection.getPassword())){
             User user = userRepository.findUserByEmailAndPassword(connection.getEmail(), connection.getPassword());
+            request.getSession().setAttribute("user", user);
             model.addAttribute("name", user.getUserName());
             model.addAttribute("email", user.getEmail());
             return "profil";
