40dc74f10833ab568f753e366839743f3fe6b425
Nigirith <fanny.streiff@gmail.com>
2019-01-08 14:16:28 +0100

    m

--- src/main/resources/static/js/Resume.js
+++ src/main/resources/static/js/Resume.js
@@ -1,3 +1,42 @@
 $( "#partie1" ).click(function() {
     $( "#tip11" ).toggle( "slow" );
+    $( "#tip12" ).toggle( "slow" );
+    $( "#tip13" ).toggle( "slow" );
+    $( "#tip14" ).toggle( "slow" );
+    $( "#tip15" ).toggle( "slow" );
+    $( "#tip16" ).toggle( "slow" );
+    $( "#tip17" ).toggle( "slow" );
+    $( "#tip18" ).toggle( "slow" );
+    $( "#tip19" ).toggle( "slow" );
+});
+
+$( "#tip11" ).click(function() {
+    $( "#tip11p" ).toggle( "slow" );
+});
+
+$( "#tip12" ).click(function() {
+    $( "#tip12p" ).toggle( "slow" );
+});
+
+$( "#tip13" ).click(function() {
+    $( "#tip13p" ).toggle( "slow" );
+});
+
+$( "#tip14" ).click(function() {
+    $( "#tip14p" ).toggle( "slow" );
+});
+$( "#tip15" ).click(function() {
+    $( "#tip15p" ).toggle( "slow" );
+});
+$( "#tip16" ).click(function() {
+    $( "#tip16p" ).toggle( "slow" );
+});
+$( "#tip17" ).click(function() {
+    $( "#tip17p" ).toggle( "slow" );
+});
+$( "#tip18" ).click(function() {
+    $( "#tip18p" ).toggle( "slow" );
+});
+$( "#tip19" ).click(function() {
+    $( "#tip19p" ).toggle( "slow" );
 });
